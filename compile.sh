791957461e09f8fff809df20dbcbeab0e247f044
shuboyang <shuboyang@yhgenomics.com>
2015-11-06 11:32:26 +0800

    linux compiling test passed

--- compile.sh
+++ compile.sh
@@ -1,9 +1,9 @@
 #!/bin/sh
 rm -rf cmakebuild/
-CMaker project:maraton head:*.h,*.hpp src:*.cpp,*.hpp,*.cc,*.c flag:-Wall-std=c++11-pthread lib:../lib/libuv.a
+CMaker project:maraton out:lib head:*.h,*.hpp src:*.cpp,*.hpp,*.cc,*.c flag:-Wall-std=c++11-pthread lib:../lib/libuv.a
 cd cmakebuild
 cmake .
 make -j 4
-cp maraton ../
+cp libmaraton.a ../lib/maraton-framework.a
+rm libmaraton.a
 cd ../
-chmod 777 maraton
